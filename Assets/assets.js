429ddc1ded73d281cc9f74d5b1bb677a4d9b625a
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-07-07 06:52:21 -0700

    feat: initial commit

---
+++ Assets/assets.js
@@ -0,0 +1,78 @@
+import Timbulogo1 from './Timbulogo1.png';
+import Timbulogo from './Timbulogo.png';
+import item from './item.png';
+import item2 from './item2.png';
+import cover from './cover.png';
+import bookcover from './bookcover.png';
+import book from './book.png';
+import HNGlogo from './HNGlogo.png';
+
+export const assets = {
+  Timbulogo1,
+  Timbulogo,
+  item,
+  item2,
+  cover,
+  bookcover,
+  book,
+  HNGlogo
+};
+
+export const timbu_data = [
+    {
+        id: 1,
+        title: 'HNG',
+        description: '7 rules of life',
+        price: 12.99,
+        image: assets.item
+    },
+    {
+        id: 2,
+        title: 'HNG',
+        description: '7 rules of life',
+        price: 12.99,
+        image: assets.item2
+    },
+    {
+        id: 3,
+        title: 'HNG',
+        description: '7 rules of life',
+        price: 12.99,
+        image: assets.item
+    },
+    {
+        id: 4,
+        title: 'HNG',
+        description: '7 rules of life',
+        price: 12.99,
+        image: assets.item2
+    },
+    {
+        id: 5,
+        title: 'HNG',
+        description: '7 rules of life',
+        price: 12.99,
+        image: assets.item
+    },
+    {
+        id: 6,
+        title: 'HNG',
+        description: '7 rules of life',
+        price: 12.99,
+        image: assets.item2
+    },
+    {
+        id: 7,
+        title: 'HNG',
+        description: '7 rules of life',
+        price: 12.99,
+        image: assets.item
+    },
+    {
+        id: 8,
+        title: 'HNG',
+        description: '7 rules of life',
+        price: 12.99,
+        image: assets.item2
+    },
+];
