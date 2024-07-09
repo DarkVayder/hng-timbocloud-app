279c10e17c9522bcc483b07353565842b29a4e00
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-07-09 09:50:00 -0700

    feat: Mobile responsiveness added

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -8,17 +8,23 @@ const Header = () => {
       <div className='relative'>
         <Image src={assets.cover} layout='responsive' objectFit='cover' alt='Cover Image' />
         <div className='absolute left-9 bottom-[-70px]'>
-          <Image src={assets.HNGlogo} width={180} height={75} alt='HNG Logo' />
+          <Image
+            src={assets.HNGlogo}
+            width={180}
+            height={75}
+            alt='HNG Logo'
+            className='w-[100px] h-auto sm:w-[150px] sm:h-auto md:w-[180px] md:h-auto'
+          />
         </div>
       </div>
 
       <div className='py-10 pt-20 px-5 md:px-12 lg:px-28 text-white'>
         <div className='max-w-4xl mx-auto md:ml-0'>
-          <h1 className='text-3xl md:text-4xl lg:text-5xl font-bold mb-4 text-left'>HNG Shop</h1>
-          <p className='text-base md:text-lg leading-snug mb-1 md:mb-2 text-left'>
+          <h1 className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-4 text-left'>HNG BookStore</h1>
+          <p className='text-sm sm:text-base md:text-lg lg:text-xl leading-snug mb-1 md:mb-2 text-left'>
             Equipping tech talents with perseverance, grit, and skills to succeed
           </p>
-          <p className='text-base md:text-lg leading-snug text-left'>
+          <p className='text-sm sm:text-base md:text-lg lg:text-xl leading-snug text-left'>
             in their tech careers.
           </p>
         </div>
--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -2,7 +2,7 @@
 
 import { assets } from '@/Assets/assets';
 import Image from 'next/image';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { RxAvatar } from 'react-icons/rx';
 import { RiArrowDropDownLine } from 'react-icons/ri';
 import { MdOutlineShoppingCart } from 'react-icons/md';
@@ -10,22 +10,10 @@ import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useCart } from '@/Context/CartContext';
 
-const Navbar = ({ isScrolled }) => {
+const Navbar = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const router = useRouter();
-  const { cartCount, updateCartCount } = useCart();
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setIsScrolled(window.scrollY > 0);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+  const { cartCount } = useCart();
 
   const toggleDropdown = () => {
     setDropdownOpen(!dropdownOpen);
@@ -41,29 +29,35 @@ const Navbar = ({ isScrolled }) => {
   };
 
   return (
-    <div className={`py-5 px-5 md:px-12 lg:px-28 bg-custom-blue flex ${isScrolled ? 'scrolled' : ''}`}>
+    <div className='py-3 px-4 md:px-12 lg:px-28 bg-custom-blue flex'>
       <div className='flex justify-between items-center w-full'>
-        <Image src={assets.Timbulogo1} width={180} height={50} alt='Timbo Logo' className='w-[130px] sm:w-auto' />
+        <Image
+          src={assets.Timbulogo1}
+          width={180}
+          height={50}
+          alt='Timbo Logo'
+          className='w-[100px] sm:w-[130px] md:w-auto'
+        />
         <div className='flex items-center space-x-4 relative'>
           <Link href='/cart'>
             <div className='relative cursor-pointer p-1 bg-white rounded-full flex items-center'>
-              <MdOutlineShoppingCart className='text-black w-8 h-8' />
-              <span className='text-black ml-2'>Cart</span>
+              <MdOutlineShoppingCart className='text-black w-6 h-6 md:w-8 md:h-8' />
+              <span className='text-black ml-2 text-sm md:text-base'>Cart</span>
               {cartCount > 0 && (
-                <span className='absolute top-0 right-0 bg-red-600 text-white rounded-full w-6 h-6 flex items-center justify-center'>
+                <span className='absolute top-0 right-0 bg-red-600 text-white rounded-full w-5 h-5 md:w-6 md:h-6 flex items-center justify-center text-xs md:text-sm'>
                   {cartCount}
                 </span>
               )}
             </div>
           </Link>
           <div className='relative flex items-center'>
-            <RxAvatar className='text-white w-8 h-8 cursor-pointer' />
+            <RxAvatar className='text-white w-6 h-6 md:w-8 md:h-8 cursor-pointer' />
             <RiArrowDropDownLine
-              className='text-white w-8 h-8 cursor-pointer'
+              className='text-white w-6 h-6 md:w-8 md:h-8 cursor-pointer'
               onClick={toggleDropdown}
             />
             {dropdownOpen && (
-              <div className='absolute right-0 mt-8 w-48 bg-white rounded-lg shadow-lg py-2 z-10'>
+              <div className='absolute right-0 mt-8 w-40 md:w-48 bg-white rounded-lg shadow-lg py-2 z-10'>
                 <p
                   className='text-gray-800 px-4 py-2 hover:bg-gray-200 cursor-pointer'
                   onClick={handleSignOut}
