35a4fdbd090c1b765881d16437473129174d52ea
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-07-08 20:56:55 -0700

    feat: Added the react-toastify for notifcation to page.jsx

--- app/summary/page.jsx
+++ app/summary/page.jsx
@@ -1,9 +1,13 @@
 'use client';
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import OrderSummary from '@/components/OrderSummary';
 import Image from 'next/image';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { assets } from "@/Assets/assets";
+import OrderSummary from '@/components/OrderSummary';
+import Navbar from '@/components/Navbar'; 
+import Footer from '@/components/Footer';
 
 const Summary = () => {
   const router = useRouter();
@@ -21,104 +25,110 @@ const Summary = () => {
   };
 
   const handleConfirmOrder = () => {
-    
-    alert('Order confirmed!');
-    router.push('/');
+    toast.success('Order confirmed!', {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+    setTimeout(() => {
+      router.push('/');
+    }, 5000);
   };
 
   return (
     <div className='bg-custom-blue min-h-screen flex flex-col'>
+      <Navbar />
+      <ToastContainer />
       <div className='container mx-auto py-5 px-5 md:px-12 lg:px-28 flex-grow'>
-        <h1 className='text-3xl font-bold text-white mb-8'>Order Summary</h1>
-
-
-        <div className='mb-8'>
-          <h2 className='text-2xl text-white font-bold'>Contact Information</h2>
-          <input type='email' placeholder='Email Address' className='w-full p-2 rounded-md' />
-        </div>
-
-
-        <div className='mb-8'>
-          <h2 className='text-2xl text-white font-bold'>Shipping Information</h2>
-          <div className='grid grid-cols-2 gap-4'>
-            <input type='text' placeholder='First Name' className='w-full p-2 rounded-md' />
-            <input type='text' placeholder='Last Name' className='w-full p-2 rounded-md' />
-            <input type='text' placeholder='Apartment' className='w-full p-2 rounded-md' />
-            <input type='text' placeholder='City' className='w-full p-2 rounded-md' />
-            <input type='text' placeholder='Country' className='w-full p-2 rounded-md' />
-            <input type='text' placeholder='State' className='w-full p-2 rounded-md' />
-            <input type='text' placeholder='Postal Code' className='w-full p-2 rounded-md' />
+        <nav className='text-white mb-4'>
+          <a href='/' className='text-blue-500 hover:underline'>Home</a> {'>'} <span>Checkout</span>
+        </nav>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-8'>
+          <div className='lg:col-span-2'>
+            <h1 className='text-3xl font-bold text-white mb-8'>Payment Details</h1>
+            <div className='mb-8'>
+              <h2 className='text-2xl text-white font-bold'>Customer Details</h2>
+              <h3 className='text-white'>Your Email Address</h3>
+              <input type='email' placeholder='e.g yourname@example.com' className='bg-inherit text-white w-full p-2 rounded-md mb-4 border border-gray-300' />
+              <h3 className='text-white'>Card Number</h3>
+              <input type='text' placeholder='1234 1234 1234 1234' className='bg-inherit text-white w-full p-2 rounded-md mb-4 border border-gray-300' />
+              <div className='grid grid-cols-1 md:grid-cols-2 gap-4 mb-4'>
+                <div>
+                  <h3 className='text-white'>Expiration Date</h3>
+                  <input type='text' placeholder='MM/YY' className='bg-inherit text-white w-full p-2 rounded-md mb-4 border border-gray-300' />
+                </div>
+                <div>
+                  <h3 className='text-white'>CVC</h3>
+                  <input type='text' placeholder='CVC' className='bg-inherit text-white w-full p-2 rounded-md mb-4 border border-gray-300' />
+                </div>
+              </div>
+              <h3 className='text-white'>Promo Code</h3>
+              <input type='text' placeholder='Your Promo Code' className='bg-inherit text-white w-full p-2 rounded-md mb-4 border border-gray-300' />
+            </div>
+            <div className='mb-8'>
+              <h2 className='text-2xl text-white font-bold'>Billing Address</h2>
+              <h3 className='text-white'>Address Line 1</h3>
+              <input type='text' placeholder='Your Address Line 1' className='bg-inherit text-white w-full p-2 rounded-md mb-4 border border-gray-300' />
+              <h3 className='text-white'>Address Line 2</h3>
+              <input type='text' placeholder='Your Address Line 2' className='bg-inherit text-white w-full p-2 rounded-md mb-4 border border-gray-300' />
+              <div className='grid grid-cols-1 md:grid-cols-2 gap-4 mb-4'>
+                <div>
+                  <h3 className='text-white'>Country</h3>
+                  <input type='text' placeholder='Your Country' className='bg-inherit text-white w-full p-2 rounded-md mb-4 border border-gray-300' />
+                </div>
+                <div>
+                  <h3 className='text-white'>State</h3>
+                  <input type='text' placeholder='Your State' className='bg-inherit text-white w-full p-2 rounded-md mb-4 border border-gray-300' />
+                </div>
+              </div>
+              <h3 className='text-white'>ZIP Code</h3>
+              <input type='text' placeholder='1234567' className='bg-inherit text-white w-full p-2 rounded-md mb-4 border border-gray-300' />
+              <div className='flex items-center'>
+                <input type='checkbox' id='saveDetails' className='mr-2' />
+                <label htmlFor='saveDetails' className='text-white'>Save details for later</label>
+              </div>
+            </div>
           </div>
-        </div>
-
- 
-        <div className='mb-8'>
-          <h2 className='text-2xl text-white font-bold'>Delivery Method</h2>
-          <div className='flex items-center mt-4'>
-            <input type='radio' id='standard' name='delivery' className='mr-2' />
-            <label htmlFor='standard' className='text-white'>Standard</label>
-            <input type='radio' id='express' name='delivery' className='ml-8 mr-2' />
-            <label htmlFor='express' className='text-white'>Express</label>
-          </div>
-        </div>
-
-
-        <div className='mb-8'>
-          <h2 className='text-2xl text-white font-bold'>Payment</h2>
-          <div className='flex items-center mt-4'>
-            <input type='radio' id='credit' name='payment' className='mr-2' />
-            <label htmlFor='credit' className='text-white'>Credit Card</label>
-            <input type='radio' id='paypal' name='payment' className='ml-8 mr-2' />
-            <label htmlFor='paypal' className='text-white'>PayPal</label>
-            <input type='radio' id='transfer' name='payment' className='ml-8 mr-2' />
-            <label htmlFor='transfer' className='text-white'>Bank Transfer</label>
-          </div>
-          <div className='mt-4'>
-            <input type='text' placeholder='Card Number' className='w-full p-2 rounded-md' />
-          </div>
-          <div className='mt-4'>
-            <input type='text' placeholder='Name on Card' className='w-full p-2 rounded-md' />
-          </div>
-        </div>
-
-
-        <div className='flex flex-col md:flex-row mt-8'>
-          <OrderSummary cartItems={cartItems} />
-
-
-          <div className='w-full md:w-2/3 mt-8 md:mt-0 md:ml-8 p-4 bg-gray-800 rounded-lg'>
-            <h2 className='text-2xl font-bold text-white'>Selected Items Summary</h2>
-            <div className='text-white mt-4'>
-              {cartItems.length === 0 ? (
-                <p>Your cart is empty.</p>
-              ) : (
-                cartItems.map(item => (
-                  <div key={item.id} className='flex items-center mb-4'>
-                    <Image src={item.image} alt={item.title} width={50} height={50} className='rounded-lg' />
-                    <div className='ml-4'>
-                      <h2 className='text-lg text-white font-bold'>{item.title}</h2>
-                      <p>${item.price} x {item.quantity}</p>
+          <div>
+            <h2 className='text-2xl font-bold text-white mb-4'>Order Summary</h2>
+            <div className='bg-gray-800 p-4 rounded-lg text-white'>
+              <div className='text-white mt-4'>
+                {cartItems.length === 0 ? (
+                  <p>Your cart is empty.</p>
+                ) : (
+                  cartItems.map(item => (
+                    <div key={item.id} className='flex items-center mb-4'>
+                      <Image src={item.image} alt={item.title} width={50} height={50} className='rounded-lg' />
+                      <div className='ml-4'>
+                        <h2 className='text-lg text-white font-bold'>{item.title}</h2>
+                        <p>${item.price} x {item.quantity}</p>
+                      </div>
                     </div>
-                  </div>
-                ))
-              )}
+                  ))
+                )}
+              </div>
+              <div className='mt-4'>
+                <p>Subtotal: ${getTotalPrice()}</p>
+                <p>Discount: $0.00</p>
+                <p>Tax: ${(getTotalPrice() * 0.07).toFixed(2)}</p>
+                <hr className='my-4' />
+                <p className='text-xl font-bold'>Total: ${(parseFloat(getTotalPrice()) + parseFloat(getTotalPrice() * 0.07)).toFixed(2)}</p>
+              </div>
+              <button
+                onClick={handleConfirmOrder}
+                className='w-full mt-4 bg-gray-700 text-white px-4 py-2 rounded-full hover:bg-gray-600 transition duration-300'
+              >
+                Confirm Order
+              </button>
             </div>
-            <div className='mt-4'>
-              <p>Subtotal: ${getTotalPrice()}</p>
-              <p>Shipping: $5.00</p>
-              <p>Taxes: $3.00</p>
-              <p className='text-xl font-bold'>Total: ${(parseFloat(getTotalPrice()) + 5.00 + 3.00).toFixed(2)}</p>
-            </div>
-            <button
-              onClick={handleConfirmOrder}
-              className='w-full mt-4 bg-gray-700 text-white px-4 py-2 rounded-full hover:bg-gray-600 transition duration-300'
-            >
-              Confirm Order
-            </button>
           </div>
         </div>
-
       </div>
+      <Footer />
     </div>
   );
 };
